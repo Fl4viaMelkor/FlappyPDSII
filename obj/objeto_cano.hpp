a7dfdbde3713fad20fde15790817f950ec760cba
GuilhermeAsafe <g.asafe.ufmg@gmail.com>
2025-05-22 23:16:24 -0300

    notas para começar

---
+++ obj/objeto_cano.hpp
@@ -0,0 +1,7 @@
+//colisão -> o objeto_passaro vai ter que identificar
+//largura, altura
+//destrutor para canos
+//gerar tres tipos de cano
+//gerar canos a partir de uma dada quantidade de frames
+
+//uma classe cano e tem um loop
