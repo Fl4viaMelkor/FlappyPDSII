384485b5872d8a84690c1f5a001edb61f610cedb
Leandro Teixeira <leandroteixeira3@gmail.com>
2025-05-25 19:34:35 -0300

    Feat: Criação da classe Dado

--- include/dados/dados.hpp
+++ include/dados/dados.hpp
@@ -10,5 +10,16 @@ using namespace std;
 #ifndef DADOS_HPP
 #define DADOS_HPP
 
+// Objeto simples a ser armazenado na base de dados. Todos os elementos são strings
+struct objeto{
+    unordered_map<string, string> dados;
+};
+
+// Implementação concreta do objeto de forma que possa ser lido e usado pelo sistema
+class Dado{
+    public:
+      void deserialize(objeto obj);
+      objeto serialize();
+};
 
 #endif //DADOS_HPP
