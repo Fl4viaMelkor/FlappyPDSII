049bd9dfa8fcd9912bbefd16e01dbe92e8d97502
guifgomes47 <guifgomes47@outlook.com>
2025-05-26 22:38:31 -0300

    Refactor: Reordenação do construtor do Player

--- src/player.cpp
+++ src/player.cpp
@@ -1,21 +1,21 @@
-#include "player.hpp"
+#include "../include/player.hpp"
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/keyboard.h>
 
 Player::Player(float x, float y, float speed, int width, int height)
-    : x(x), y(y), speed(speed), width(width), height(height),dead(false),velY(0), gravidade(0.5f) {}
+    : dead(false), velY(0), gravidade(0.5f), noChao(false), x(x), y(y), speed(speed), width(width), height(height) {}
 
 void Player::update(const ALLEGRO_KEYBOARD_STATE& key_state) {
     if (al_key_down(&key_state, ALLEGRO_KEY_UP))    y -= speed;
     if (al_key_down(&key_state, ALLEGRO_KEY_DOWN))  y += speed;
-    
-    
+
+
     // Pulo
     if (al_key_down(&key_state, ALLEGRO_KEY_SPACE)) {
         velY = -5.0f;  // força do pulo
-    
+
     }
 
     // Gravidade
