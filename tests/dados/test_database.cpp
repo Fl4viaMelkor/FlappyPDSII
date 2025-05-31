b4bc60f27187c5d2bb333f62d6f3ceb847d3d4e7
Leandro Teixeira <leandroteixeira3@gmail.com>
2025-05-31 20:01:58 -0300

    Tests: Doctest.h Configurado

---
+++ tests/dados/test_database.cpp
@@ -0,0 +1,10 @@
+//
+// Created by leand on 31/05/2025.
+//
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "../doctest.h"
+
+TEST_CASE("Teste Factorial - Caso base"){
+    CHECK(1+1 == 3);
+}
