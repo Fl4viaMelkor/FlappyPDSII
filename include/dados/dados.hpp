b1ec0bed8d20a8b340b729c324b5f87cd0d021a0
swords <76114949+swords-0@users.noreply.github.com>
2025-06-22 17:06:23 -0300

    config pra tela e musica essas outras coisas

--- include/dados/dados.hpp
+++ include/dados/dados.hpp
@@ -2,16 +2,15 @@
 // Created by leand on 25/05/2025.
 //
 
+
 #ifndef DADOS_HPP
 #define DADOS_HPP
 
-#include <any>
-#include <iterator> // Para std::begin e std::end, embora muitas vezes n├úo seja estritamente necess├írio para cont├¬ineres padr├úo como vector/map/unordered_map
-#include <map>
 #include <string>
-#include <unordered_map>
-#include <utility> // Para std::pair, e indiretamente para begin/end em alguns contextos
+#include <map>
 #include <vector>
+#include <any>
+
 using namespace std;
 
 // Objeto simples a ser armazenado na base de dados. Todos os elementos são strings
@@ -23,38 +22,34 @@ struct objeto {
 
 // Implementação concreta do objeto de forma que possa ser lido e usado pelo sistema
 class Dado {
-  public:
-    virtual ~Dado() = default;
-    virtual void carregar(objeto obj) = 0;
-    virtual objeto exportar() = 0;
-    virtual bool operator==(const Dado &outro) const = 0;
-    virtual bool operator!=(const Dado &outro) const = 0;
+    public:
+        virtual void set(string chave, const std::any &valor) = 0;
+        virtual any get(string chave) = 0;
+        virtual ~Dado() = default;
+        virtual void carregar(objeto obj) = 0;
+        virtual objeto exportar() = 0;
+        virtual bool operator==(const Dado &outro) const = 0;
+        virtual bool operator!=(const Dado &outro) const = 0;
 };
 
 // Dado jogador que representa um jogador padrão
 class Dado_Jogador : public Dado {
-  protected:
-    string nome_, apelido_;
-    vector<int> pontuacoes_;
+    protected:
+        string nome_, apelido_;
+        int vitorias_, derrotas_;
+        vector<int> pontuacoes_;
 
-  public:
-    Dado_Jogador();
-    explicit Dado_Jogador(string nome, string apelido = "");
-    explicit Dado_Jogador(objeto obj);
-    void carregar(objeto obj) override;
-    objeto exportar() override;
-    bool operator==(const Dado &outro) const override;
-    bool operator!=(const Dado &outro) const override;
-    void apelido(const string &apelido);
-    string apelido() const;
-    vector<int> pontuacoes() const;
-    void pontuacoes(const vector<int> &pontuacoes);
-    string nome() const;
-    void nome(const string &nome);
-
-    static unordered_map<string, string> get_sql_columns();
-    static vector<string> get_colunas();
-    static string get_primary_key();
+    public:
+        Dado_Jogador();
+        Dado_Jogador(string nome, string apelido = "", int vitorias = 0, int derrotas = 0);
+        Dado_Jogador(objeto obj);
+        void carregar(objeto obj) override;
+        objeto exportar() override;
+        bool operator==(const Dado &outro) const;
+        bool operator!=(const Dado &outro) const;
+        void set(string chave, const any &valor) override;
+        any get(string chave) override;
 };
 
-#endif // DADOS_HPP
+
+#endif //DADOS_HPP
