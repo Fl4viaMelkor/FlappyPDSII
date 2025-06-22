c03fd92f3dc42cc89a087b045f7697008ac0ac8e
swords <76114949+swords-0@users.noreply.github.com>
2025-06-22 17:11:23 -0300

    Update config.hpp

--- include/dados/config.hpp
+++ include/dados/config.hpp
@@ -5,8 +5,7 @@
 const int LARGURA_JANELA = 1280;
 const int ALTURA_JANELA = 720;
 
-// Esta é a resolução "interna" do seu jogo.
-// Todas as suas telas (Inicial, Jogo, etc.) vão desenhar neste espaço.
+//resolução interna
 const int LARGURA_NATIVA = 800;
 const int ALTURA_NATIVA = 600;
 
