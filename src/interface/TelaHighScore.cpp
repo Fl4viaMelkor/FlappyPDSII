a5864cb1475ce49e3119e964d5d687111f05eaee
swords <76114949+swords-0@users.noreply.github.com>
2025-06-23 15:21:40 -0300

    Update TelaHighScore.cpp
    add dados

--- src/interface/TelaHighScore.cpp
+++ src/interface/TelaHighScore.cpp
@@ -2,9 +2,9 @@
 #include <allegro5/allegro_primitives.h>
 #include <stdexcept>
 
-TelaHighScores::TelaHighScores(GerenciadorHighScores& manager, int largura, int altura) :
+TelaHighScores::TelaHighScores(PlayerLogger& logger_ref, int largura, int altura) :
     fonte_titulo(nullptr), fonte_scores(nullptr),
-    gerenciador_scores(manager),
+    logger(logger_ref),
     proxima_tela_estado(EstadoProximaTela::NENHUM),
     largura_tela(largura), altura_tela(altura) {
         
@@ -26,20 +26,23 @@ void TelaHighScores::draw() {
     //texto
     al_draw_text(fonte_titulo, al_map_rgb(255, 215, 0), centro_x, altura_tela * 0.1f, ALLEGRO_ALIGN_CENTER, "RECORDES");
 
-    const auto& scores = gerenciador_scores.getScores();
-    float pos_y_inicial = altura_tela * 0.25f;
-    float espacamento = 40.0f;
-//texto
-    if (scores.empty()) {
+    std::string scores_formatados = logger.listar_dados_ordenados(": ", "\n", "\n\n");
+    
+    if (scores_formatados.empty()) {
         al_draw_text(fonte_scores, al_map_rgb(255,255,255), centro_x, altura_tela * 0.5f, ALLEGRO_ALIGN_CENTER, "Nenhum recorde ainda!");
     } else {
-        for (size_t i = 0; i < scores.size(); ++i) {
-            std::string texto_score = std::to_string(i + 1) + ". " + scores[i].nome + " - " + std::to_string(scores[i].score);
-            al_draw_text(fonte_scores, al_map_rgb(255,255,255), centro_x, pos_y_inicial + i * espacamento, ALLEGRO_ALIGN_CENTER, texto_score.c_str());
-        }
+        
+        al_draw_multiline_text(
+            fonte_scores,
+            al_map_rgb(255, 255, 255),
+            centro_x, altura_tela * 0.25f,  // Posição inicial do texto
+            largura_tela * 0.9, 40.0f,       // Largura máxima da caixa de texto e altura da linha
+            ALLEGRO_ALIGN_CENTER,            // Alinhamento do texto
+            scores_formatados.c_str()
+        );
     }
     
-    al_draw_text(fonte_scores, al_map_rgb(200,200,200), centro_x, altura_tela * 0.9f, ALLEGRO_ALIGN_CENTER, "Pressione ESC para voltar");
+    al_draw_text(fonte_scores, al_map_rgb(200, 200, 200), centro_x, altura_tela * 0.9f, ALLEGRO_ALIGN_CENTER, "Pressione ESC para voltar");
 }
 //vai pra proxima tela
 void TelaHighScores::step(ALLEGRO_EVENT& evento) {
