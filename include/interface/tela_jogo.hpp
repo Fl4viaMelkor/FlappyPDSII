0572571cab9f9291dd9221c520f68471351dfe39
leandroTeixeira <leandroteixeira3@gmail.com>
2025-06-24 22:42:05 -0300

    bug: Bug colisão

--- include/interface/tela_jogo.hpp
+++ include/interface/tela_jogo.hpp
@@ -1,42 +1,19 @@
 #ifndef TELAJOGO_HPP
 #define TELAJOGO_HPP
 
+#include "../fisica/fisica.hpp"
 #include "../objeto_cano.hpp"
 #include "../player.hpp"
 #include "../util.hpp"
-#include "../fisica/fisica.hpp"
 #include "parallax_background.hpp"
 #include "tela_base.hpp"
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
 
-// Caso algum item ou coisa seja adicionado na tela de JOGO deve criar um ponteiro para esse objeto no PRIVATE
-
-// class TelaJogo : public TelaBase {
-// private:
-//     Player* player;
-//
-//     Cano* MySquare; //teste
-//     ALLEGRO_KEYBOARD_STATE keyState;
-//     bool end;   //Possível controlador para fim da tela
-//
-// public:
-//     TelaJogo();
-//     ~TelaJogo();
-//
-//     void update() override;
-//     void draw() override;
-//     void step(ALLEGRO_EVENT& evento) override;
-//
-//     bool acabouJogo() const;
-// };
-
-//
-
-enum class GameState{
-  PAUSED_FOR_START, // Pausado aguardando o input do jogador para começar
-  PLAYING, //Jogo em andamento
-  GAME_OVER
+enum class GameState {
+    PAUSED_FOR_START, // Pausado aguardando o input do jogador para começar
+    PLAYING,          // Jogo em andamento
+    GAME_OVER
 };
 
 class TelaJogo : public TelaBase, public RetanguloHitbox {
@@ -44,10 +21,10 @@ class TelaJogo : public TelaBase, public RetanguloHitbox {
     gravidade g{ 2, 3 };
     Player *player;
 
-    //adicionando vector
-    std::vector<Cano*> canos;  // substitui MySquare
+    // adicionando vector
+    std::vector<Cano *> canos; // substitui MySquare
 
-    //Cano *MySquare; *trocado pelo vector
+    // Cano *MySquare; *trocado pelo vector
 
     ALLEGRO_KEYBOARD_STATE keyState;
     ParallaxBackground *parallaxBg;
@@ -57,7 +34,7 @@ class TelaJogo : public TelaBase, public RetanguloHitbox {
     Detector_Colisao *detector;
 
     GameState currentGameState;
-    ALLEGRO_FONT* font;
+    ALLEGRO_FONT *font;
 
   public:
     TelaJogo();
@@ -73,7 +50,11 @@ class TelaJogo : public TelaBase, public RetanguloHitbox {
     void resetProximaTelaEstado() override;
 
     bool colisao(coordenadas p) override { return !noInterior(p); }
-    void onCollision() override { cout << "Colisão aconteceu " << endl; }
+    void onCollision() override
+    {
+        end = true;
+        cout << "Colisão aconteceu " << endl;
+    }
     int pontos;
 };
 
--- src/player.cpp
+++ src/player.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
 #include "../include/player.hpp"
 #include "../include/sprite.hpp"
+#include "player_exception.hpp"
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/keyboard.h>
-#include "player_exception.hpp"
+#include <iostream>
 
-//Created By Flávia
+// Created By Flávia
 
 Player::Player(const std::string &filename, float x, float y, float speed, float width, float height)
   : RetanguloHitbox({ x, y }, width, height)
@@ -22,20 +22,19 @@ Player::Player(const std::string &filename, float x, float y, float speed, float
     try {
         sprite = new SpriteAnimado(filename, 3, 24, 24, 8.0f);
         carregouSprite = true;
-    } catch (const std::runtime_error &e) {
+    }
+    catch (const std::runtime_error &e) {
         std::cerr << "Aviso: " << e.what() << std::endl;
         carregouSprite = false;
     }
 }
 
-
-Player::~Player() {
-    delete sprite;
-}
+Player::~Player() { delete sprite; }
 
 void Player::update(const ALLEGRO_KEYBOARD_STATE &key_state)
 {
-    if (!IsAlive) return;
+    if (!IsAlive)
+        return;
 
     float currentTime = al_get_time();
     float deltaTime = currentTime - ultimoTempo;
@@ -51,40 +50,33 @@ void Player::update(const ALLEGRO_KEYBOARD_STATE &key_state)
     velY += gravidade;
     ponto_inferior_esquerdo.y += velY;
     gravidade += 0.0001;
+    jumpCooldown -= 0.0001;
 
     if (carregouSprite && sprite)
         sprite->update(deltaTime);
 }
 
-void Player::draw() {
+void Player::draw()
+{
     if (carregouSprite && sprite) {
         sprite->draw(ponto_inferior_esquerdo, false);
-    } else {
+    }
+    else {
         // Desenha retângulo vermelho se sprite não carregou
-        al_draw_filled_rectangle(
-            ponto_inferior_esquerdo.x,
-            ponto_inferior_esquerdo.y,
-            ponto_inferior_esquerdo.x + base,
-            ponto_inferior_esquerdo.y + altura,
-            al_map_rgb(255, 0, 0)
-        );
+        al_draw_filled_rectangle(ponto_inferior_esquerdo.x, ponto_inferior_esquerdo.y, ponto_inferior_esquerdo.x + base,
+                                 ponto_inferior_esquerdo.y + altura, al_map_rgb(255, 0, 0));
     }
 }
 
-bool Player::colisao(coordenadas p) {
-    return noPerimetro(p) || noInterior(p);
-}
+bool Player::colisao(coordenadas p) { return noPerimetro(p) || noInterior(p); }
 
-void Player::Kill() {
+void Player::Kill()
+{
     IsAlive = false;
     velY = 0;
     speed = 0;
 }
 
-bool Player::getIsAlive() const {
-    return IsAlive;
-}
+bool Player::getIsAlive() const { return IsAlive; }
 
-void Player::onCollision(){ 
-    Kill();
-}
+void Player::onCollision() { Kill(); }
